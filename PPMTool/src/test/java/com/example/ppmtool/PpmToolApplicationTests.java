20808b874f4f8e5ae198d52d3b9c07d3cf7bd014
Aditi Venugopalan <aditi.venu1@gmail.com>
2019-06-04 15:48:41 -0400

    first commit

--- PPMTool/src/test/java/com/example/ppmtool/PpmToolApplicationTests.java
+++ PPMTool/src/test/java/com/example/ppmtool/PpmToolApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PpmtoolApplicationTests {
+public class PpmToolApplicationTests {
 
 	@Test
 	public void contextLoads() {
